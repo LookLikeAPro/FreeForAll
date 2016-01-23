a85dc7b3fa7475cf4dd6b051a99bd319648969b0
looklikeapro <jerryzhou00@gmail.com>
2016-01-22 22:53:09 -0500

    init

---
+++ android/src/com/jerryz/freeforall/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jerryz.freeforall;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jerryz.freeforall.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ html/src/com/jerryz/freeforall/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jerryz.freeforall.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jerryz.freeforall.Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Game();
+        }
+}
